e58071df9af7b01662a4995563d5b2a356278532
Andreas Demenagas <ademenagas01@gmail.com>
2019-11-10 19:43:59 +0200

    Initial Commit

---
+++ HotelMovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotelMovieApp
+//
+//  Created by Andrew Demenagas on 10/11/19.
+//  Copyright © 2019 Andrew Demenagas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

dcd57f4c57d9c9048e3400e510398fac883edc77
Andreas Demenagas <ademenagas01@gmail.com>
2020-05-03 20:22:41 +0300

    Clean workspace

--- HotelMovieApp/SceneDelegate.swift
+++ HotelMovieApp/SceneDelegate.swift
@@ -22,7 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.windowScene = windowScene
         window?.makeKeyAndVisible()
         
-        window?.rootViewController = MainTabBarController()
+        window?.rootViewController = UIViewController()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {

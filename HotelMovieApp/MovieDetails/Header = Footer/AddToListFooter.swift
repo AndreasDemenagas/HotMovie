9fc9e24a03cd8fd8b0d5953f8a93b3360a62e384
Andreas Demenagas <ademenagas01@gmail.com>
2020-05-12 17:15:36 +0300

    Footer File

---
+++ HotelMovieApp/MovieDetails/Header = Footer/AddToListFooter.swift
@@ -0,0 +1,22 @@
+//
+//  AddToListFooter.swift
+//  HotelMovieApp
+//
+//  Created by Andrew Demenagas on 12/5/20.
+//  Copyright © 2020 Andrew Demenagas. All rights reserved.
+//
+
+import UIKit
+
+class AddToListFooter: UICollectionReusableView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    
+}
